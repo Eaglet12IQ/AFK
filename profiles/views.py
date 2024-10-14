99f60c72cd5f44b0e281ba54d60ef3545fd80b94
Eaglet12IQ <eaglet12iq@gmail.com>
2024-10-14 14:10:02 +0300

    second fix after merge

--- profiles/views.py
+++ profiles/views.py
@@ -4,4 +4,10 @@ from profiles.models import Profile
 
 def profile_view(request, user_id):
     profile = get_object_or_404(Profile, user_id=user_id)
-    return render(request, 'profile.html',  {'profile': profile})
+    user = get_object_or_404(User, id=user_id)
+    context = {
+        "nickname": profile.nickname,
+        "date_joined": user.date_joined,
+        "user_id": user_id,
+    }
+    return render(request, 'profile.html',  context)
