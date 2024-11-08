c9f851cb93719c4f9a406879b916fe957474274e
Eaglet12IQ <eaglet12iq@gmail.com>
2024-11-08 17:13:54 +0300

    multiple selection of difficulty

--- taskGenerator/views.py
+++ taskGenerator/views.py
@@ -6,19 +6,19 @@ import random
 
 def idea_generation(request):
     interests = json.loads(request.GET.get('interests'))
-    difficulty = request.GET.get('difficulty')
+    difficulty = json.loads(request.GET.get('difficulty'))
     last_generations = json.loads(request.GET.get('last_generations'))
 
     last_generations_id_list = [key for gen in last_generations for key in gen.keys()]
 
-    if not interests and difficulty is None:
+    if not interests and not difficulty:
         tasks = Tasks.objects.filter().exclude(id__in=last_generations_id_list)
     elif not interests:
-        tasks = Tasks.objects.filter(difficulty=difficulty).exclude(id__in=last_generations_id_list)
-    elif difficulty is None:
+        tasks = Tasks.objects.filter(difficulty__in=difficulty).exclude(id__in=last_generations_id_list)
+    elif not difficulty:
         tasks = Tasks.objects.filter(interest__in=interests).exclude(id__in=last_generations_id_list)
     else:
-        tasks = Tasks.objects.filter(difficulty=difficulty, interest__in=interests).exclude(id__in=last_generations_id_list)
+        tasks = Tasks.objects.filter(difficulty__in=difficulty, interest__in=interests).exclude(id__in=last_generations_id_list)
 
     random_task = random.choice(tasks)
 
