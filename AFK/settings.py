8b2eb270bfcf164abcb74e7f48666270752bf07a
LeoCristt <SergeySav009@yandex.ru>
2024-12-15 13:09:45 +0300

    fix for prod

--- AFK/settings.py
+++ AFK/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mt1!vgpq77=0)12+5+6a$cr8d(s7w6%uy%62+77g3%wh(yqlrr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['afk.progfiks.ru', 'www.afk.progfiks.ru', '37.140.192.35']
 
 
 # Application definition
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'productionfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
