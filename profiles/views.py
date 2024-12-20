8929e1557280391864b57162d3511a36eef21c2c
Eaglet12IQ <eaglet12iq@gmail.com>
2024-12-20 13:51:00 +0300

    access to the settings of a third-party user is blocked

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from authentication.models import User
 from profiles.models import Profile
 from taskGenerator.models import completedTask
@@ -34,14 +34,17 @@ def profile_view(request, user_id):
 
 
 def settings_view(request, user_id):
-    profile = get_object_or_404(Profile, user_id=user_id)
-    user = get_object_or_404(User, id=user_id)
-    context = {
-        "nickname": profile.nickname,
-        "email": user.email,
-        "profile_picture": profile.profile_picture,
-    }
-    return render(request, 'settings.html', context)
+    if user_id == request.user.id:
+        profile = get_object_or_404(Profile, user_id=user_id)
+        user = get_object_or_404(User, id=user_id)
+        context = {
+            "nickname": profile.nickname,
+            "email": user.email,
+            "profile_picture": profile.profile_picture,
+        }
+        return render(request, 'settings.html', context)
+    else:
+       return redirect("main")
 
 def profile_settings_change(request, user_id):
     return Profile.settings_change(request)
